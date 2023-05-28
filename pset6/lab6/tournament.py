bd9161ee1810149236f9e6d17913617f324fb2e6
Rodrigo do Vale Stankowicz <121521674+rodrigodovalestankowicz@users.noreply.github.com>
2023-05-28 00:07:54 -0300

    simulate_tournament() terminado

--- pset6/lab6/tournament.py
+++ pset6/lab6/tournament.py
@@ -1,71 +1,73 @@
-# Simulate a sports tournament
-
 import csv
-from csv import DictReader
 import sys
 import random
 
-# Number of simluations to run -> quantos torneios vão acontecer
 N = 1000
 
 def main():
-    # Ensure correct usage
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
     teams = dict({})
-    # Ler do arquivo csv todos os times e guardar numa dict com "Nome do time" : rating
-
     counts = dict({})
-    # Outro dict, mas com "Nome do time" : quantos torneios ele ganhou
 
     with open(sys.argv[1], "r") as file:
-        tmp_teams = DictReader(file)
+        tmp_teams = csv.DictReader(file)
 
         for linha in tmp_teams:
             teams[linha[('team')]] = int(linha[('rating')])
             counts[linha[('team')]] = int(0)
 
-    print(list(teams.keys()))
+    simulate_tournament(teams)
 
-    simulate_tournament(list(teams.keys()))
+    # ATUALIZAR AS WINS NO COUNTS, SENDO N O NÚMERO DE RODADAS
 
-    # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 
-def simulate_game(team1, team2):
-    # Simula um jogo selecionando, randomicamente, quem ganha 
-    """Simulate a game. Return True if team1 wins, False otherwise."""
-    rating1 = team1["rating"]
-    rating2 = team2["rating"]
+def simulate_game(rating1, rating2):
+    # rating1 = team1["rating"]
+    # rating2 = team2["rating"]
     probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
     return random.random() < probability
 
 
 def simulate_round(teams):
-    # Pega uma lista de times e simula (randomicamente) quais deles vão ganhar, retornando uma lista de vencedores do round atual
-    """Simulate a round. Return a list of winning teams."""
     winners = []
+    temp_teams = list([])
 
-    # Simulate games for all pairs of teams
+    for i in teams:
+        temp_teams.append(i)
+
+    # DAR UM JEITO DE ENVIAR O ('TEAM' : 12312) PARA DENTRO DE SIMULATE_GAME() function, AO INVÉS DE ENVIAR APENAS O RATING CORRESPONDENTE -> só arrumar a gambiarra 
     for i in range(0, len(teams), 2):
-        if simulate_game(teams[i], teams[i + 1]):
-            winners.append(teams[i])
+        if simulate_game(teams[temp_teams[i]], teams[temp_teams[i + 1]]):
+            winners.append(temp_teams[i])
         else:
-            winners.append(teams[i + 1])
+            winners.append(temp_teams[i + 1])
 
     return winners
 
 
 def simulate_tournament(teams):
-    # Simula o torneio inteiro usando simulate_round, que, por sua vez, usa simulate_game
-    # Entra uma lista com times (assumindo, que a quantidade de times tem base 2)
-    """Simulate a tournament. Return name of winning team."""
-    for i in range(len(teams)):
-        # games = simulate_round(teams)
-        # print(games)
-        print()
+    winners = simulate_round(teams)
+    tmp = dict({})
+
+    while len(winners) > 1:
+        for i in range(len(winners)):
+            tmp[winners[i]] = teams[winners[i]]
+
+        teams.clear()
+
+        for i in range(len(winners)):
+            teams[winners[i]] = tmp[winners[i]]
+
+        tmp.clear()
+
+        winners.clear()
+        winners = simulate_round(teams)
+
+    print(winners)
 
 if __name__ == "__main__":
     main()
